bb2536d83a5829d0b7c440a7f552d12d7d64d44c
Michael Thiesen <micthiesen@gmail.com>
2018-07-28 21:41:38 -0700

    Use eslint

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-const express = require('express');
-const path = require('path');
-const request = require('request');
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 /* database setup */
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const { Pool } = require('pg')
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 
 /* express app setup */
-const app = express();
-app.use('/static', express.static(path.join(__dirname, 'static')));
-app.use('/static', express.static(path.join(__dirname, '../node_modules')));
-app.use('/vue', express.static(path.join(__dirname, 'vue')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express()
+app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use('/static', express.static(path.join(__dirname, '../node_modules')))
+app.use('/vue', express.static(path.join(__dirname, 'vue')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 /* view setup */
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => res.render('pages/index'))
 app.get('/db', async (req, res) => {
   try {
-    const client = await pool.connect();
-    const results = await client.query('SELECT * FROM test_table');
-    res.render('pages/db', { results: results.rows });
-    client.release();
+    const client = await pool.connect()
+    const results = await client.query('SELECT * FROM test_table')
+    res.render('pages/db', { results: results.rows })
+    client.release()
   } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
+    console.error(err)
+    res.send('Error ' + err)
   }
-});
+})
 
 /* wait for requests */
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
