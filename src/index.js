35a380d171b443f9a86309bf2b03cc8419c56734
Michael Thiesen <micthiesen@gmail.com>
2018-07-29 21:53:12 -0700

    Use redis as session store if available

--- src/index.js
+++ src/index.js
@@ -3,20 +3,29 @@ const assert = require('assert')
 const express = require('express')
 const path = require('path')
 const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 const views = require('./views')
 const PORT = process.env.PORT || 5000
 
 assert.ok(process.env.DATABASE_URL)
-assert.ok(process.env.SSS_SECRET_KEY)
 assert.ok(process.env.SET_STATUSES_SLEEP_INTERVAL)
+assert.ok(process.env.SSS_SECRET_KEY)
 
 /* express app setup */
 const app = express()
-app.use(session({
+const sessionOpts = {
   resave: false,
   saveUninitialized: false,
   secret: process.env.SSS_SECRET_KEY
-}))
+}
+if (process.env.REDISCLOUD_URL) {
+  sessionOpts.store = new RedisStore({
+    url: process.env.REDISCLOUD_URL
+  })
+} else {
+  console.warn('Falling back to memory session storage')
+}
+app.use(session(sessionOpts))
 app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use('/static', express.static(path.join(__dirname, '../node_modules')))
 app.use('/vue', express.static(path.join(__dirname, 'vue')))
