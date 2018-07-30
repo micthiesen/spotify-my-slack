6c2d839a926a7c3fc0c9637898c3ddbf4102c90b
Michael Thiesen <micthiesen@gmail.com>
2018-07-30 16:49:58 -0700

    Use async/await with Slack auth

--- src/views/slack-grant-callback.js
+++ src/views/slack-grant-callback.js
@@ -2,25 +2,24 @@ const { WebClient } = require('@slack/client')
 
 const web = new WebClient()
 
-module.exports = function (req, res) {
+module.exports = async function (req, res) {
   if (!req.query.code) {
     console.warn('Couldn\'t get grant code from Slack')
     res.redirect('/')
     return
   }
 
-  web.oauth.access({
-    client_id: process.env.SLACK_CLIENT_ID,
-    client_secret: process.env.SLACK_CLIENT_SECRET,
-    code: req.query.code,
-    redirect_uri: process.env.SLACK_REDIRECT_URI
-  })
-    .then((slackRes) => {
-      req.session.slackAccessToken = slackRes.access_token
-      res.redirect('/')
+  try {
+    const slackRes = await web.oauth.access({
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code,
+      redirect_uri: process.env.SLACK_REDIRECT_URI
     })
-    .catch((err) => {
-      console.warn(err)
-      res.redirect('/')
-    })
+    req.session.slackAccessToken = slackRes.access_token
+  } catch (err) {
+    console.warn(err)
+  }
+
+  res.redirect('/')
 }
