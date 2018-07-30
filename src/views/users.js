984b0be09e6f1a43c4c3c005a136db11abfd8225
Michael Thiesen <micthiesen@gmail.com>
2018-07-30 16:52:28 -0700

    Use async/await with users page

--- src/views/users.js
+++ src/views/users.js
@@ -1,12 +1,11 @@
 const models = require('../models')
 
-module.exports = function (req, res) {
-  models.User.findAll()
-    .then(users => {
-      res.render('users', { users: users })
-    })
-    .catch(err => {
-      console.error(err)
-      res.send('Error ' + err)
-    })
+module.exports = async function (req, res) {
+  try {
+    const users = await models.User.findAll()
+    res.render('users', { users: users })
+  } catch (err) {
+    console.error(err)
+    res.redirect('/')
+  }
 }
