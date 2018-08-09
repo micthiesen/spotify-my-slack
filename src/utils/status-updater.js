93a46839a489f13ee99cf977a20f9996ed6e6709
Michael Thiesen <micthiesen@gmail.com>
2018-08-08 23:45:50 -0700

    Do it in parallel

--- src/utils/status-updater.js
+++ src/utils/status-updater.js
@@ -6,49 +6,58 @@ const { WebClient } = require('@slack/client')
 module.exports.updateStatuses = async function () {
   const timerStart = Date.now()
   const users = await models.User.findAll()
-  var successes = 0
-  var skips = 0
-  var failures = 0
 
-  for (const user of users) {
-    const spotifyClient = await spotify.getUserClient(user)
+  const updatePromises = users.map((user) => {
+    return new Promise(async (resolve) => {
+      return performUpdate(user, resolve)
+    })
+  })
 
-    // retrieve Spotify plater state
-    try {
-      var playerData = await spotifyClient.getMyCurrentPlaybackState()
-    } catch (err) {
-      console.warn(`Retrieving Spotify player state failed for user ${user.id}:`, err)
-      failures += 1
-      continue
-    }
-
-    // update Slack status
-    try {
-      if (playerData.body.is_playing) {
-        await setUserStatus(user, playerData.body.item)
-        successes += 1
-      } else if (user.statusSetLastTime) {
-        await clearUserStatus(user)
-        successes += 1
-      } else {
-        skips += 1
-      }
-    } catch (err) {
-      if (err.data && err.data.error === 'token_revoked') {
-        console.log(`Token revoked for user ${user.id}. Deleting user...`)
-        try { await user.destroy() } catch (err) { console.error(err) }
-        skips += 1
-      } else {
-        console.warn(`Updating Slack status failed for user ${user.id}:`, err)
-        failures += 1
-      }
-    }
-  }
+  const statsList = await Promise.all(updatePromises)
+  const stats = statsList.reduce((memo, statItem) => {
+    memo[statItem] += 1
+    return memo
+  }, {success: 0, failure: 0, skip: 0})
 
   const timerEnd = Date.now()
   const timeSpent = (timerEnd - timerStart) / 1000
 
-  console.log(`Updated ${successes} of ${users.length} users (${skips} skipped, ${failures} failed) [${timeSpent}s]`)
+  console.log(`Updated ${stats.success} of ${users.length} users (${stats.skip} skipped, ${stats.failure} failed) [${timeSpent}s]`)
+}
+
+// update a single user
+const performUpdate = async (user, resolve) => {
+  const spotifyClient = await spotify.getUserClient(user)
+
+  // retrieve Spotify plater state
+  try {
+    var playerData = await spotifyClient.getMyCurrentPlaybackState()
+  } catch (err) {
+    console.warn(`Retrieving Spotify player state failed for user ${user.id}:`, err)
+    return resolve('failure')
+  }
+
+  // update Slack status
+  try {
+    if (playerData.body.is_playing) {
+      await setUserStatus(user, playerData.body.item)
+      return resolve('success')
+    } else if (user.statusSetLastTime) {
+      await clearUserStatus(user)
+      return resolve('success')
+    } else {
+      return resolve('skip')
+    }
+  } catch (err) {
+    if (err.data && err.data.error === 'token_revoked') {
+      console.log(`Token revoked for user ${user.id}. Deleting user...`)
+      try { await user.destroy() } catch (err) { console.error(err) }
+      return resolve('skip')
+    } else {
+      console.warn(`Updating Slack status failed for user ${user.id}:`, err)
+      return resolve('failure')
+    }
+  }
 }
 
 // helper functions
