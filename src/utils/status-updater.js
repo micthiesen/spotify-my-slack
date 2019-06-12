0e74356f053b97109f55b7a25eab6d04dcaed4a6
Michael <micthiesen@gmail.com>
2019-06-12 11:31:52 -0700

    Update status-updater.js

--- src/utils/status-updater.js
+++ src/utils/status-updater.js
@@ -14,8 +14,8 @@ module.exports.updateLoop = async function (userId) {
 
     const updatePromise = new Promise(async (resolve) => { return performUpdate(user, resolve) })
     var interval = await updatePromise[1]
-  } catch (err) {
-    console.trace(`Fatal error in update loop for user ${userId}: {err}`)
+  } catch {
+    console.trace(`Fatal error in update loop for user ${userId}`)
   }
 
   setTimeout(() => { module.exports.updateLoop(userId) }, interval || process.env.UPDATE_LOOP_DEFAULT_INTERVAL)
