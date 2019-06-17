b67e586af63ff260a746c0e34d1a33cd984bb1c9
Michael <micthiesen@gmail.com>
2019-06-17 10:30:53 -0700

    Update status-updater.js

--- src/utils/status-updater.js
+++ src/utils/status-updater.js
@@ -33,8 +33,7 @@ const performUpdate = async (user, resolve) => {
       const retryAfter = parseInt(err.headers['retry-after']) + 2
       return resolve(['skip', retryAfter * 1000])
     } else if (err.statusCode === 401) {
-      console.log(`401 response received from Spotify. Deleting user ${user.id}`)
-      try { await user.destroy() } catch (err) { console.error(err) }
+      console.warn(`401 response received from Spotify. Ignoring...`)
       return resolve(['failure', null])
     } else {
       console.warn(`Retrieving Spotify player state failed for user ${user.id}:`, err)
