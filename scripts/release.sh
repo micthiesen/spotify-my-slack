c396abd65084cf63db1d5e188e68ea671e54a5bd
Michael Thiesen <micthiesen@gmail.com>
2019-12-08 06:05:20 -0800

    Add a release phase

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,4 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 cd "$SCRIPT_DIR/../server"
-npm install --only=prod && npm install --only=dev
-npm run build
 ./node_modules/.bin/sequelize db:migrate
