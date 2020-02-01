4fd29c0ae008e99dc3bf05718f6f3f029bdbc82e
Michael Thiesen <micthiesen@gmail.com>
2020-02-01 15:21:04 -0800

    Mmmmm

--- backend/worker.py
+++ backend/worker.py
@@ -8,10 +8,16 @@ import logging
 LOGGER = logging.getLogger("backend")
 
 
+async def do_fake_work(index, sleep):
+    LOGGER.info(f"Finished work for {index}")
+    await asyncio.sleep(sleep)
+
+
 async def worker_entrypoint() -> None:
     """
     The entrypoint for the worker. Currently a stub
     """
     while True:
         LOGGER.info("In the worker function")
-        await asyncio.sleep(15)
+        tasks = (do_fake_work(i, 10) for i in range(2))
+        await asyncio.gather(*tasks)
