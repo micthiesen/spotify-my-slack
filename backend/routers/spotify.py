9c7a9dcf29dc998e0509e4b1fa3545ff0827698e
Michael Thiesen <micthiesen@gmail.com>
2020-02-01 17:08:48 -0800

    Refactor Spotify auth a bit

--- backend/routers/spotify.py
+++ backend/routers/spotify.py
@@ -1,15 +1,14 @@
 """
 Spotify OAuth grant routes
 """
-from typing import Optional, cast
+from typing import Optional
 from urllib.parse import urlencode
 
-import httpx
 from fastapi.routing import APIRouter
-from pydantic import BaseModel, ValidationError
 from starlette.responses import RedirectResponse
 
 from backend.conf import LOGGER, SETTINGS
+from backend.utils.spotify import TokenExchangeError, exchange_code_for_tokens
 
 
 AUTHORIZE_SCOPES = [
@@ -17,20 +16,7 @@ AUTHORIZE_SCOPES = [
     "user-read-playback-state",
 ]
 AUTHORIZE_URI = "https://accounts.spotify.com/authorize"
-TOKEN_EXCHANGE_URI = "https://accounts.spotify.com/api/token"
 ROUTER = APIRouter()
-
-
-class TokenExchangeData(BaseModel):
-    """
-    Data returned by Spotify after exchanging tokens
-    """
-
-    access_token: str
-    token_type: str
-    scope: str
-    expires_in: int
-    refresh_token: str
 
 
 @ROUTER.get("/spotify-grant")
@@ -60,34 +46,13 @@ async def spotify_grant_callback(
     if code is None and error is None:
         error = f"Neither 'code' nor 'error' were provided from Spotify"
     if error is not None:
-        LOGGER.info("Spotify grant callback error: %s", error)
-        return RedirectResponse("/")
-
-    exchange_args = {
-        "grant_type": "authorization_code",
-        "code": code,
-        "redirect_uri": SETTINGS.spotify_redirect_uri,
-        "client_id": SETTINGS.spotify_client_id,
-        "client_secret": SETTINGS.spotify_client_secret,
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(TOKEN_EXCHANGE_URI, data=exchange_args)
-    if response.status_code != 200:
-        LOGGER.info(
-            "Unexpected %s response from Spotify when exchanging tokens: %s",
-            response.status_code,
-            response.text,
-        )
+        LOGGER.warning("Spotify grant callback error: %s", error)
         return RedirectResponse("/")
 
     try:
-        exchange_json = cast(dict, response.json())
-        exchange_data = TokenExchangeData(**exchange_json)
-    except (ValidationError, ValueError) as err:
-        LOGGER.info(
-            "Could not decode response from Spotify when exchaning tokens: %s",
-            err,
-        )
+        exchange_data = await exchange_code_for_tokens(code=code)
+    except TokenExchangeError as err:
+        LOGGER.warning("%s", err)
         return RedirectResponse("/")
 
     # TODO save the access token to the database
--- backend/utils/spotify.py
+++ backend/utils/spotify.py
@@ -1,3 +1,60 @@
 """
 Spotify utilities
 """
+from typing import cast
+
+import httpx
+from pydantic import BaseModel, ValidationError
+
+from backend.conf import SETTINGS
+
+
+TOKEN_EXCHANGE_URI = "https://accounts.spotify.com/api/token"
+
+
+class TokenExchangeError(Exception):
+    """
+    Error when exchaging tokens with Spotify
+    """
+
+
+class TokenExchangeData(BaseModel):
+    """
+    Data returned by Spotify after exchanging tokens
+    """
+
+    access_token: str
+    token_type: str
+    scope: str
+    expires_in: int
+    refresh_token: str
+
+
+async def exchange_code_for_tokens(code: str) -> TokenExchangeData:
+    """
+    Exchange an authorization code for an access & refresh token
+    """
+    exchange_args = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": SETTINGS.spotify_redirect_uri,
+        "client_id": SETTINGS.spotify_client_id,
+        "client_secret": SETTINGS.spotify_client_secret,
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(TOKEN_EXCHANGE_URI, data=exchange_args)
+    if response.status_code != 200:
+        raise TokenExchangeError(
+            f"Unexpected {response.status_code} response from Spotify when "
+            f"exchanging tokens: {response.text}",
+        )
+
+    try:
+        exchange_json = cast(dict, response.json())
+        exchange_data = TokenExchangeData(**exchange_json)
+    except (ValidationError, ValueError) as err:
+        raise TokenExchangeError(
+            f"Could not decode response from Spotify when exchanging tokens: "
+            f"{err}",
+        )
+    return exchange_data
