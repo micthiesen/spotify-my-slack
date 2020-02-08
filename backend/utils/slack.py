9dc7f53d018fe06a95dca02ba62df9033444aa9f
Michael Thiesen <micthiesen@gmail.com>
2020-02-08 15:29:27 -0800

    Set status in Slack

--- backend/utils/slack.py
+++ backend/utils/slack.py
@@ -1,6 +1,9 @@
 """
 Slack utilities
 """
+import json
+from typing import Optional
+
 from pydantic import BaseModel
 
 from backend.config import SETTINGS
@@ -14,6 +17,7 @@ class SlackApiError(BaseApiError):
 
 
 MAKE_REQUEST = gen_make_request("Slack", SlackApiError)
+USERS_PROFILE_SET_URI = "https://slack.com/api/users.profile.set"
 TOKEN_EXCHANGE_URI = "https://slack.com/api/oauth.access"
 
 
@@ -43,3 +47,45 @@ async def get_new_access_token(code: str) -> TokenExchangeData:
     return await MAKE_REQUEST(
         "POST", TOKEN_EXCHANGE_URI, TokenExchangeData, data=exchange_args,
     )
+
+
+class UserProfileArgs(BaseModel):
+    """
+    Options when updating a Slack user profile
+    """
+
+    status_text: str
+    status_emoji: str
+    status_expiration: Optional[int] = 0
+
+
+class UserProfileData(BaseModel):
+    """
+    Data returned by Slack after updating a profile
+    """
+
+    ok: bool
+    error: Optional[str]
+
+
+async def set_status(
+    user_profile_args: UserProfileArgs, access_token: str
+) -> UserProfileData:
+    """
+    Set the user's status
+    """
+    user_profile_data = await MAKE_REQUEST(
+        "POST",
+        USERS_PROFILE_SET_URI,
+        UserProfileData,
+        access_token=access_token,
+        data={
+            "profile": json.dumps(user_profile_args.dict()).encode("UTF-8"),
+        },
+    )
+    if not user_profile_data.ok:
+        raise SlackApiError(
+            f"200 response but error returned: {user_profile_data.error}",
+            error_code=422,
+        )
+    return user_profile_data
