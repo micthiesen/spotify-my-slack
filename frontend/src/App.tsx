b37a7f9fa9947b7a20fb29e1ea3a87ace3a8829d
Michael Thiesen <micthiesen@gmail.com>
2020-04-25 03:40:05 -0700

    Start using blueprint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavbarGroup, NavbarHeading } from '@blueprintjs/core';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bp3-dark">
+      <Navbar>
+        <NavbarGroup>
+          <NavbarHeading>Spotify my Slack</NavbarHeading>
+        </NavbarGroup>
+      </Navbar>
     </div>
   );
 }
